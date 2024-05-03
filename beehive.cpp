cf203109405af97de19d5f24690cfaeb009e416b
arael <jonasg@umich.edu>
2024-05-03 15:36:03 -0400

    format printed window list better

--- beehive.cpp
+++ beehive.cpp
@@ -10,18 +10,18 @@ using std::cout, std::cin, std::string;
 
 uint32_t select_window(uint32_t max_window_count)
 {
-    uint32_t window_id = max_window_count+1;
+    uint32_t window_idx = max_window_count+1;
 
-    while (window_id < 1 || window_id > max_window_count) { // While invalid input
+    while (window_idx < 1 || window_idx > max_window_count) { // While invalid input
         cout << "Enter window index to stream from [1 - " << max_window_count << "]: ";
-        if (cin >> window_id) continue;
+        if (cin >> window_idx) continue;
         // Bad input. Clear error and input buffer
         cin.clear();
         string dmy;
         cin >> dmy;
     }
 
-    return window_id-1; // Convert to 0-based index
+    return window_idx-1; // Convert to 0-based index
 }
 
 int main()
@@ -55,14 +55,15 @@ int main()
         if (window_layer > 1) continue; /* Skip windows that are not at the top layer. */
 
         CFStringRef window_name_ref = (CFStringRef) CFDictionaryGetValue(window_info_ref, kCGWindowOwnerName);
-        cout << CFStringGetCStringPtr(window_name_ref, kCFStringEncodingUTF8) << '\n';
-        ++active_window_count;
+        active_window_count++;
+        cout << active_window_count << ". " << CFStringGetCStringPtr(window_name_ref, kCFStringEncodingUTF8) << '\t';
     }
+    cout << '\n';
 
     CFRelease(window_infolist_ref);
 
-    uint32_t selected_window = select_window(active_window_count);
-    cout << "Selected window: " << selected_window << '\n';
+    uint32_t selected_window_idx = select_window(active_window_count);
+    cout << "Selected window: " << selected_window_idx << '\n';
 #endif /* __APPLE__ */
 
     /* TODO 2. Expose a port */
